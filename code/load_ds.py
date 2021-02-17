0230da1ea521a33aa3f44c28ea0aca29bae04402
Andy Connolly <andrew.c.connolly@dartmouth.edu>
2021-02-17 13:58:31 -0500

    added load ds

---
+++ code/load_ds.py
@@ -0,0 +1,33 @@
+from amvpa import *
+import sys
+
+sub = sys.argv[1]
+
+dpath = "../../preproc/sub-rid0000{}/".format(sub)
+opath = "../sub-rid0000{}/".format(sub)
+
+tasks = ["beh","tax"]
+data_fn = dpath+"Qtstats_{}_run-{}.nii.gz"
+mask_fn = dpath+"glasser_masks.nii.gz"
+animals = ['bird','insect','primate','reptile','ungulate']
+behaviors = ['eating','fighting','running','swimming']
+twenty_conds = ['{}_{}'.format(a,b) for a in animals for b in behaviors]
+
+## Pre-calculated searchlight map
+sl_map_fn = "glassGM_Searchlight.txt"
+
+# END SETUP 
+
+ds = None
+for task in tasks:
+    for r in range(1,6):
+        if ds is None:
+            ds = Dataset(data_fn.format(task, r), mask=mask_fn)
+        else:
+            ds.append(Dataset(data_fn.format(task,r), mask=mask_fn))
+
+ds.set_sa('targets', np.tile(twenty_conds,10))
+ds.set_sa('chunks', np.repeat(range(10),20))
+
+
+
